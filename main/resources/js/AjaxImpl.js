0e7f0b6b18242bc61874666af4a785cbcb0debdc
fjgonzalezm <fgonzalezm@gmail.com>
2011-01-17 00:46:36 -0800

    Added Ajax test case and initial framework functionality

--- main/resources/js/AjaxImpl.js
+++ main/resources/js/AjaxImpl.js
@@ -3,11 +3,15 @@
  */
 
 var AjaxUtil = JsonUtil = (function($,wnd,undefined){
-	var settings = {},
+	var initialized = false,
 		ajaxQueue = [],
 		retryQueue = [],
 		inProgressXhr = [],
+		maxRetry = 3,
+		_timeout = 290,
 		_addAjaxQueue = function(options) {
+			if (!initialized) _init();
+			
 			if (options===undefined)
 				return;
 			
@@ -17,6 +21,8 @@ var AjaxUtil = JsonUtil = (function($,wnd,undefined){
 		// When called, it will fireup a ajax call for each elem in ajaxQueue
 		//
 		_runAjax = function() {
+			if (!initialized) _init();
+
 			if (ajaxQueue===undefined || !ajaxQueue.length || ajaxQueue.length<1)
 				return;
 			
@@ -29,16 +35,47 @@ var AjaxUtil = JsonUtil = (function($,wnd,undefined){
 		// Settings it's a jQuery's Ajax object
 		//
 		_runNow = function(settings) {
+			if (!initialized) _init();
+
 			if (settings===undefined)
 				return;
 			
-			
+			$.ajax({
+				url: settings.url,
+				success: function(data, status, xhr) {
+					if (settings.success) {
+						settings['data'] = data;
+						settings['status'] = xhr.status;
+						settings['xhr'] = xhr;
+						settings.success(settings);
+					}
+				},
+				error: function(xhr, status, error) {
+					if(settings.doRetry) {
+						if (settings.retry===undefined)
+							settings.retry=0;
+							
+						settings.retry +=1;
+						if (settings.retry<=maxRetry)
+							retryQueue.push(settings);	
+					}
+					
+					if (settings.error) {
+						settings['status'] = xhr.status;
+						settings['xhr'] = xhr;
+						settings['error'] = error;
+						settings.error(settings);
+					}
+				}
+			});
 			
 		}, // End runNow
 		//
 		// Returns a HTML attribute formatted as: attrName = 'attrValue'
 		// 
 		_getAttr = function(options){
+			if (!initialized) _init();
+
 			return (options===undefined || options.attrName===undefined || options.attrValue===undefined) 
 					? ""
 					: options.attrName===undefined
@@ -47,6 +84,8 @@ var AjaxUtil = JsonUtil = (function($,wnd,undefined){
 		}, //End toAttr
 		
 		_getHtmlElem = function(options){
+			if (!initialized) _init();
+
 			var def = {
 				elemName: "div"
 			};
@@ -58,10 +97,14 @@ var AjaxUtil = JsonUtil = (function($,wnd,undefined){
 		}, // End toHtmlElem
 		
 		_getAjaxQueue = function() {
+			if (!initialized) _init();
+
 			return ajaxQueue;
 		}, // End _getAjaxQueue
 		
 		_getFacets = function(resume, ajaxWrapper){
+			if (!initialized) _init();
+
 			if (!resume) {
 				// Fire up Ajax call
 				
@@ -76,8 +119,25 @@ var AjaxUtil = JsonUtil = (function($,wnd,undefined){
 					
 				}
 			}
-		}
+		},
+		_init = function() {
+			if (initialized) 
+				return;
 				
+			$.ajaxSetup({
+				timeout: _timeout,
+				dataType: 'json',
+				async: true,
+				beforeSend: function(xhr) {
+					xhr.setRequestHeader("Header1","Value1");
+					xhr.setRequestHeader("Header2","Value2");
+					xhr.setRequestHeader("Header3","Value3");
+				}
+			});
+			initialized = true;
+		};
+		
+		
 		return {
 			getAttr: _getAttr,
 			getHtmlElem: _getHtmlElem,
--- test/resources/js/AjaxImplTest.js
+++ test/resources/js/AjaxImplTest.js
@@ -4,33 +4,29 @@
 
 (function(){
  	var obj = {
-		url: "/contacts.json",
+		url: "/qunit/test/resources/json/contacts.json",
 		success: function(ajaxWrapper) {
-			$(document.body).append(JsonUtil.getHtmlElem({
-				elemName: "div",
-				attrName: "id",
-				attrValue: "testDiv"
-			})).find("#testDiv").append(ajaxWrapper.data);
+			ok(true, 'Ajax call successful');
 		},
 		error: function(ajaxWrapper) {
-			$(document.body).append(JsonUtil.getHtmlElem({
-				elemName: "div",
-				attrName: "id",
-				attrValue: "testDiv"
-			})).find("#testDiv").append(ajaxWrapper.data);
+			alert(ajaxWrapper.status);
+			ok(false,'Ajax call failed');
 		},
+		dataType: 'json',
+		async: true,
 		doRetry: false
 	};
 	
+	QUnit.init();
 
 	module('AjaxUtil.getHtmlElem() Test Module');
 	
 	test('Use Case: Called without arguments', function() {
-		equals("<div />",JsonUtil.getHtmlElem(),'Passed');
+		equals("<div />",AjaxUtil.getHtmlElem(),'Passed');
 	 });
 
 	test('Use Case: Call with all parameters especified', function() {
-		equals("<p id='testId'>Test Content</p>",JsonUtil.getHtmlElem({
+		equals("<p id='testId'>Test Content</p>",AjaxUtil.getHtmlElem({
 			elemName: "p",
 			elemContent: "Test Content",
 			attrName: "id",
@@ -39,14 +35,14 @@
 	 });
 
 	test('Use Case: Call with Attribute both parameters missing', function() {
-		equals("<p >Test Content</p>",JsonUtil.getHtmlElem({
+		equals("<p >Test Content</p>",AjaxUtil.getHtmlElem({
 			elemName: "p",
 			elemContent: "Test Content"
 		}),'Use Case: Call with Attribute both parameters missing');
 	 });
 
 	test('Use Case: Call with Attribute Value parameter missing', function() {
-		equals("<p >Test Content</p>",JsonUtil.getHtmlElem({
+		equals("<p >Test Content</p>",AjaxUtil.getHtmlElem({
 			elemName: "p",
 			elemContent: "Test Content"
 		}),'Use Case: Call with Attribute Value parameter missing');
@@ -55,23 +51,23 @@
 
 	module('AjaxUtil.getAttr() Module');
 	test('Use Case: Call with no parameter', function() {
-		equals(JsonUtil.getAttr(),"",'Use Case: Call with no parameter');
+		equals(AjaxUtil.getAttr(),"",'Use Case: Call with no parameter');
 	 });
 
 	test('Use Case: Call with Attribute Name parameter missing', function() {
-		equals(JsonUtil.getAttr({
+		equals(AjaxUtil.getAttr({
 			attrValue: "testId"
 		}),"",'Use Case: Call with Attribute Name parameter missing');
 	 });
 
 	test('Use Case: Call with Attribute Value parameter missing', function() {
-		equals(JsonUtil.getAttr({
+		equals(AjaxUtil.getAttr({
 			attrName: "class"
 		}),"",'Use Case: Call with Attribute Value parameter missing');
 	 });
 
 	test('Use Case: Call with all Attribute Values provided', function() {
-		equals(JsonUtil.getAttr({
+		equals(AjaxUtil.getAttr({
 			attrName: "class",
 			attrValue: "active"
 		}),"class='active'",'Use Case: Call with all Attribute Values provided');
@@ -80,11 +76,11 @@
 
 	module('AjaxUtil.addAjaxQueue() Module');
 
-	JsonUtil.addAjaxQueue(obj);
+	AjaxUtil.addAjaxQueue(obj);
 
 	test('Use Case: Validates the object added is the object returned',function() {
 		equals(
-			JsonUtil.getAjaxQueue()[0],
+			AjaxUtil.getAjaxQueue()[0],
 			obj,
 			'Use Case: Validates the object added is the object returned'
 		);
@@ -92,22 +88,22 @@
 
 	test('Use Case: Validate length is correct',function() {
 		equals(
-			JsonUtil.getAjaxQueue().length,
+			AjaxUtil.getAjaxQueue().length,
 			1,
 			'Use Case: Validate length is correct'
 		);
 	});
 
 	
-	//module('AjaxUtil.runNow() Module');
-	//JsonUtil.getAjaxQueue().pop();
-	//	test('JsonUtil.runNow() Test Cases',function() {
-	//		stop(3000);
-	//		
-	//		equals(
-	//			JsonUtil.runNow(),'','Use Case: Success Ajax call'
-	//		);
-	//	});
+	module('AjaxUtil.runNow() Module');
+	//AjaxUtil.getAjaxQueue().pop();
+	test('Retrieve a json resources Async',function() {
+		stop();
+		
+		AjaxUtil.runNow(obj);
+		
+		setTimeout(function(){start()},300);
+	});
 	
 })();
  

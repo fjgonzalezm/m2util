3fe96d13f3e6bf770141b0ea979549380c5fb100
fjgonzalezm <fgonzalezm@gmail.com>
2011-01-17 23:36:19 -0800

    Changelog: - Check-in fix for Issue#2 - Rename runNow to doAjaxNow to be more self-explanatory - Implemented retry mechanism for Ajax calls that failed - Added 1 more test case for retry and enhanced few others

--- main/resources/js/AjaxImpl.js
+++ main/resources/js/AjaxImpl.js
@@ -1,150 +1,166 @@
 /**
  * @author fjgm
  */
-
-var AjaxUtil = JsonUtil = (function($,wnd,undefined){
-	var initialized = false,
-		ajaxQueue = [],
-		retryQueue = [],
-		inProgressXhr = [],
-		maxRetry = 3,
-		_timeout = 290,
-		_addAjaxQueue = function(options) {
-			if (!initialized) _init();
-			
-			if (options===undefined)
-				return;
-			
-			ajaxQueue.push(options);
-		}, // End addAjaxQueue
-		//
-		// When called, it will fireup a ajax call for each elem in ajaxQueue
-		//
-		_runAjax = function() {
-			if (!initialized) _init();
-
-			if (ajaxQueue===undefined || !ajaxQueue.length || ajaxQueue.length<1)
-				return;
-			
-			
-		}, // End runAjax
+var AjaxUtil = JsonUtil = (function($, wnd, undefined){
+    var initialized = false, 
+		ajaxQueue = [], 
+		retryQueue = [], 
+		inProgressXhr = [], 
+		_maxRetry = 3, 
+		_timeout = 290, 
 		
+		_addAjaxQueue = function(options){
+        if (!initialized) 
+            _init();
+        
+        if (options === undefined) 
+            return;
+        
+        ajaxQueue.push(options);
+    }, // End addAjaxQueue
+ _runAjax = function(){
+        if (!initialized) 
+            _init();
+        
+        if (ajaxQueue === undefined || !ajaxQueue.length || ajaxQueue.length < 1) 
+            return;
+        
+        
+    }, // End runAjax
+    //
+    // Execute a ajax call immediately based on settings provided.
+    // Settings it's a jQuery's Ajax object
+    //
+    _doAjaxNow = function(settings){
+        if (!initialized) {
+			_init();
+		}
 		
-		//
-		// Execute a ajax call immediately based on settings provided.
-		// Settings it's a jQuery's Ajax object
-		//
-		_runNow = function(settings) {
-			if (!initialized) _init();
+        if (settings === undefined) {
+            return;
+        }
+		
+		var _wrapper = settings;
+		
+        (function _selfCallback(_callback){
+            if (_callback) {
+                if (_wrapper.status == 200) { // Should never reach this but call the callback function in any case 
+                    _wrapper.success(_wrapper);
+                    return;
+                }
+                else {
+                    if (_wrapper.doRetry) {
+                        if (_wrapper.retry === undefined) {
+							_wrapper.retry = 0;
+						}
+                        _wrapper.retry++;
+						
+						if (_wrapper.retry >= _maxRetry) {
+							_wrapper.error(_wrapper);
+							return;
+						}
 
-			if (settings===undefined)
-				return;
-			
-			$.ajax({
-				url: settings.url,
-				success: function(data, status, xhr) {
-					if (settings.success) {
-						settings['data'] = data;
-						settings['status'] = xhr.status;
-						settings['xhr'] = xhr;
-						settings.success(settings);
+                    } else {
+						//_wrapper.retry++; // Workaround for adjust count of retry which is off by 1
+						_wrapper.error(_wrapper);
+						return;
 					}
-				},
-				error: function(xhr, status, error) {
-					if(settings.doRetry) {
-						if (settings.retry===undefined)
-							settings.retry=0;
-							
-						settings.retry +=1;
-						if (settings.retry<=maxRetry)
-							retryQueue.push(settings);	
-					}
-					
-					if (settings.error) {
-						settings['status'] = xhr.status;
-						settings['xhr'] = xhr;
-						settings['error'] = error;
-						settings.error(settings);
-					}
-				}
-			});
+                }
+                
+            }
 			
-		}, // End runNow
-		//
-		// Returns a HTML attribute formatted as: attrName = 'attrValue'
-		// 
-		_getAttr = function(options){
-			if (!initialized) _init();
-
-			return (options===undefined || options.attrName===undefined || options.attrValue===undefined) 
-					? ""
-					: options.attrName===undefined
-					? ""
-					: options.attrName + "='" + options.attrValue + "'"; 
-		}, //End toAttr
+            $.ajax({
+                url: _wrapper.url,
+                success: function(data, status, xhr){
+                    _wrapper['data'] = data;
+                    _wrapper['status'] = xhr.status;
+					_wrapper['statusText'] = status;
+                    _wrapper['xhr'] = xhr;
+                    _wrapper.success(_wrapper);
+                },
+                error: function(xhr, status, error) {
+                    _wrapper['status'] = xhr.status != 200 ? xhr.status : 404;
+					_wrapper['statusText'] = status;
+                    _wrapper['xhr'] = xhr;
+                    _wrapper['err'] = error;
+                    _selfCallback(_wrapper);
+                }
+            });
+			
+        })();
+        
+    }, // End runNow
+ _getAttr = function(options){
+        if (!initialized) 
+            _init();
+        
+        return (options === undefined || options.attrName === undefined || options.attrValue === undefined) ? "" : options.attrName === undefined ? "" : options.attrName + "='" + options.attrValue + "'";
+    }, //End toAttr
+ _getHtmlElem = function(options){
+        if (!initialized) 
+            _init();
+        
+        var def = {
+            elemName: "div"
+        };
+        def = $.extend(def, options);
+        return def.elemContent === undefined ? "\<" + def.elemName + " " + _getAttr(def) + "\/>" : "\<" + def.elemName + " " + _getAttr(def) + "\>" +
+        def.elemContent +
+        "\<\/" +
+        def.elemName +
+        "\>";
+    }, // End toHtmlElem
+ _getAjaxQueue = function(){
+        if (!initialized) 
+            _init();
+        
+        return ajaxQueue;
+    }, // End _getAjaxQueue
+ _getFacets = function(resume, ajaxWrapper){
+        if (!initialized) 
+            _init();
+        
+        if (!resume) {
+            // Fire up Ajax call
+        
+        }
+        else {
+            // resume Ajax call from where I left off (It's acting as a callback function)
+            // Extract data from within ajaxWrapper
+            if (!ajaxWrapper || ajaxWrapper.status != 200) { // Failed
+                if (ajaxWrapper.ajaxObj && ajaxWrapper.ajaxObj.isRetry) {
+                    retryQueue.push(ajaxWrapper.ajaxObj);
+                }
+            }
+            else { //Succeeded
+            }
+        }
+    }, _init = function(){
+        if (initialized) {
+			return;
+		} 
 		
-		_getHtmlElem = function(options){
-			if (!initialized) _init();
-
-			var def = {
-				elemName: "div"
-			};
-			def = $.extend(def, options);
-			return def.elemContent===undefined	
-					? "\<" + def.elemName + " " + _getAttr(def)+ "\/>"
-					: "\<" + def.elemName + " " + _getAttr(def) +"\>"
-						+ def.elemContent + "\<\/" + def.elemName + "\>";
-		}, // End toHtmlElem
-		
-		_getAjaxQueue = function() {
-			if (!initialized) _init();
-
-			return ajaxQueue;
-		}, // End _getAjaxQueue
-		
-		_getFacets = function(resume, ajaxWrapper){
-			if (!initialized) _init();
-
-			if (!resume) {
-				// Fire up Ajax call
-				
-			} else {
-				// resume Ajax call from where I left off (It's acting as a callback function)
-				// Extract data from within ajaxWrapper
-				if (!ajaxWrapper || ajaxWrapper.status!=200) { // Failed
-					if (ajaxWrapper.ajaxObj && ajaxWrapper.ajaxObj.isRetry) {
-						retryQueue.push(ajaxWrapper.ajaxObj);
-					}
-				} else { //Succeeded
-					
-				}
-			}
-		},
-		_init = function() {
-			if (initialized) 
-				return;
-				
-			$.ajaxSetup({
-				timeout: _timeout,
-				dataType: 'json',
-				async: true,
-				beforeSend: function(xhr) {
-					xhr.setRequestHeader("Header1","Value1");
-					xhr.setRequestHeader("Header2","Value2");
-					xhr.setRequestHeader("Header3","Value3");
-				}
-			});
-			initialized = true;
-		};
-		
-		
-		return {
-			getAttr: _getAttr,
-			getHtmlElem: _getHtmlElem,
-			addAjaxQueue: _addAjaxQueue,
-			getAjaxQueue: _getAjaxQueue,
-			runAjax: _runAjax,
-			runNow: _runNow
-		}; 	
-})(jQuery,this);
+        $.ajaxSetup({
+            timeout: _timeout,
+            dataType: 'json',
+            async: true,
+            beforeSend: function(xhr){
+                xhr.setRequestHeader("Header1", "Value1");
+                xhr.setRequestHeader("Header2", "Value2");
+                xhr.setRequestHeader("Header3", "Value3");
+            }
+        });
+        initialized = true;
+    };
+    
+    
+    return {
+        getAttr: _getAttr,
+        getHtmlElem: _getHtmlElem,
+        addAjaxQueue: _addAjaxQueue,
+        getAjaxQueue: _getAjaxQueue,
+        runAjax: _runAjax,
+        doAjaxNow: _doAjaxNow
+    };
+})(jQuery, this);
 
--- test/resources/js/AjaxImplTest.js
+++ test/resources/js/AjaxImplTest.js
@@ -1,126 +1,150 @@
 /**
  * @author fjgm
  */
-
 (function(){
- 	var obj = {
-		url: "/qunit/test/resources/json/contacts.json",
-		success: function(ajaxWrapper) {
-			ok(ajaxWrapper.status==200, 'Ajax call successful');
-		},
-		error: function(ajaxWrapper) {
-			ok(ajaxWrapper.status!=200,'Ajax call failed');
-		},
-		dataType: 'json',
-		async: true,
-		doRetry: false
-	};
-	
-	QUnit.init();
-
-	module('AjaxUtil.getHtmlElem() Test Module');
-	
-	test('Use Case: Called without arguments', function() {
-		equals("<div />",AjaxUtil.getHtmlElem(),'Passed');
-	 });
-
-	test('Use Case: Call with all parameters especified', function() {
-		equals("<p id='testId'>Test Content</p>",AjaxUtil.getHtmlElem({
-			elemName: "p",
-			elemContent: "Test Content",
-			attrName: "id",
-			attrValue: "testId"
-		}),'Use Case: Call with all parameters especified');
-	 });
-
-	test('Use Case: Call with Attribute both parameters missing', function() {
-		equals("<p >Test Content</p>",AjaxUtil.getHtmlElem({
-			elemName: "p",
-			elemContent: "Test Content"
-		}),'Use Case: Call with Attribute both parameters missing');
-	 });
-
-	test('Use Case: Call with Attribute Value parameter missing', function() {
-		equals("<p >Test Content</p>",AjaxUtil.getHtmlElem({
-			elemName: "p",
-			elemContent: "Test Content"
-		}),'Use Case: Call with Attribute Value parameter missing');
-	 });
-
-
-	module('AjaxUtil.getAttr() Module');
-	test('Use Case: Call with no parameter', function() {
-		equals(AjaxUtil.getAttr(),"",'Use Case: Call with no parameter');
-	 });
-
-	test('Use Case: Call with Attribute Name parameter missing', function() {
-		equals(AjaxUtil.getAttr({
-			attrValue: "testId"
-		}),"",'Use Case: Call with Attribute Name parameter missing');
-	 });
-
-	test('Use Case: Call with Attribute Value parameter missing', function() {
-		equals(AjaxUtil.getAttr({
-			attrName: "class"
-		}),"",'Use Case: Call with Attribute Value parameter missing');
-	 });
-
-	test('Use Case: Call with all Attribute Values provided', function() {
-		equals(AjaxUtil.getAttr({
-			attrName: "class",
-			attrValue: "active"
-		}),"class='active'",'Use Case: Call with all Attribute Values provided');
-	 });
-
-
-	module('AjaxUtil.addAjaxQueue() Module');
-
-	AjaxUtil.addAjaxQueue(obj);
-
-	test('Use Case: Validates the object added is the object returned',function() {
-		equals(
-			AjaxUtil.getAjaxQueue()[0],
-			obj,
-			'Use Case: Validates the object added is the object returned'
-		);
-	});
-
-	test('Use Case: Validate length is correct',function() {
-		equals(
-			AjaxUtil.getAjaxQueue().length,
-			1,
-			'Use Case: Validate length is correct'
-		);
-	});
-
-	
-	module('AjaxUtil.runNow() Module');
-	//AjaxUtil.getAjaxQueue().pop();
-	test('Retrieve a json resources Async',function() {
-		stop();
-		
-		expect(1);
-		AjaxUtil.runNow(obj);
-		
-		setTimeout(function(){start()},300);
-	});
-
-	test('Retrieve an unexisting json resources Async',function() {
-		stop();
-		
-		expect(1);
-		obj['url']='/qunit/test/resources/json/doesexists.json';
-		obj['success'] = function() {
-			ok(false,'Test an unsuccessful Ajax call');
-		};
-		obj['error'] = function(ajaxWrapper) {
-			ok(ajaxWrapper.status!=200,'Test a unsuccessful Ajax call');	
-		};
-						
-		AjaxUtil.runNow(obj);
-		
-		setTimeout(function(){start()},300);
-	});
-	
+    var obj = {
+        url: "/qunit/test/resources/json/contacts.json",
+        success: function(ajaxWrapper){
+            ok(ajaxWrapper.status == 200, 'Ajax call successful');
+        },
+        error: function(ajaxWrapper){
+            ok(ajaxWrapper.status != 200, 'Ajax call failed');
+            // ok(ajaxWrapper.retry>3,'Retry executed 3 times');
+        },
+        dataType: 'json',
+        async: true,
+        doRetry: true
+    };
+    
+    QUnit.init();
+    
+    module('AjaxUtil.getHtmlElem() Test Module');
+    
+    test('Use Case: Called without arguments', function(){
+        equals("<div />", AjaxUtil.getHtmlElem(), 'Passed');
+    });
+    
+    test('Use Case: Call with all parameters especified', function(){
+        equals("<p id='testId'>Test Content</p>", AjaxUtil.getHtmlElem({
+            elemName: "p",
+            elemContent: "Test Content",
+            attrName: "id",
+            attrValue: "testId"
+        }), 'Use Case: Call with all parameters especified');
+    });
+    
+    test('Use Case: Call with Attribute both parameters missing', function(){
+        equals("<p >Test Content</p>", AjaxUtil.getHtmlElem({
+            elemName: "p",
+            elemContent: "Test Content"
+        }), 'Use Case: Call with Attribute both parameters missing');
+    });
+    
+    test('Use Case: Call with Attribute Value parameter missing', function(){
+        equals("<p >Test Content</p>", AjaxUtil.getHtmlElem({
+            elemName: "p",
+            elemContent: "Test Content"
+        }), 'Use Case: Call with Attribute Value parameter missing');
+    });
+    
+    
+    module('AjaxUtil.getAttr() Module');
+    test('Use Case: Call with no parameter', function(){
+        equals(AjaxUtil.getAttr(), "", 'Use Case: Call with no parameter');
+    });
+    
+    test('Use Case: Call with Attribute Name parameter missing', function(){
+        equals(AjaxUtil.getAttr({
+            attrValue: "testId"
+        }), "", 'Use Case: Call with Attribute Name parameter missing');
+    });
+    
+    test('Use Case: Call with Attribute Value parameter missing', function(){
+        equals(AjaxUtil.getAttr({
+            attrName: "class"
+        }), "", 'Use Case: Call with Attribute Value parameter missing');
+    });
+    
+    test('Use Case: Call with all Attribute Values provided', function(){
+        equals(AjaxUtil.getAttr({
+            attrName: "class",
+            attrValue: "active"
+        }), "class='active'", 'Use Case: Call with all Attribute Values provided');
+    });
+    
+    
+    module('AjaxUtil.addAjaxQueue() Module');
+    
+    AjaxUtil.addAjaxQueue(obj);
+    
+    test('Use Case: Validates the object added is the object returned', function(){
+        equals(AjaxUtil.getAjaxQueue()[0], obj, 'Use Case: Validates the object added is the object returned');
+    });
+    
+    test('Use Case: Validate length is correct', function(){
+        equals(AjaxUtil.getAjaxQueue().length, 1, 'Use Case: Validate length is correct');
+    });
+    
+    
+    module('AjaxUtil.runNow() Module');
+    test('Retrieve a json resources Async', function(){
+        stop();
+        
+        expect(1);
+        obj['success'] = function(ajaxWrapper){
+            ok(ajaxWrapper.status == 200, 'HTTP Code = 200');
+        };
+        
+        AjaxUtil.doAjaxNow(obj);
+        
+        setTimeout(function(){
+            start()
+        }, 300);
+    });
+    
+    test('Retrieve an unexisting json resources Async with no retry', function(){
+        stop();
+        
+        expect(2);
+        
+        obj['url'] = '/qunit/test/resources/json/doesnotexist.json';
+        obj['success'] = function(ajaxWrapper){
+            ok(false, 'Test an unsuccessful Ajax call');
+        };
+        obj['error'] = function(ajaxWrapper){
+            ok(ajaxWrapper.status != 200, 'HTTP Code different from 200');
+            ok(ajaxWrapper.retry === undefined, 'No retry executed');
+        };
+        
+        obj['doRetry'] = false;
+        
+        AjaxUtil.doAjaxNow(obj);
+        
+        setTimeout(function(){
+            start()
+        }, 300);
+    });
+    
+    test('Retrieve an unexisting json resources Async and _retry_ enabled', function(){
+        stop();
+        
+        expect(2);
+        obj['url'] = '/qunit/test/resources/json/doesnotexist.json';
+        obj['success'] = function(ajaxWrapper){
+            ok(false, 'Expecting call to purposely fail.');
+        };
+        obj['error'] = function(ajaxWrapper){
+            ok(ajaxWrapper.status != 200, 'HTTP Code different from 200');
+            ok(ajaxWrapper.retry == 3, 'Retries reached 3+');
+        };
+        
+        obj['doRetry'] = true;
+        
+        AjaxUtil.doAjaxNow(obj);
+        
+        setTimeout(function(){
+            start()
+        }, 300);
+    });
+    
 })();
- 
